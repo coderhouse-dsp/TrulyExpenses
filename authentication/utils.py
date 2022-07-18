ee49ee590d9f00e6117299c7448ccd7d69acca1b
coderhouse-dsp <dspdeep222@gmail.com>
2022-07-18 19:25:10 +0530

    Tut15 completed

--- authentication/utils.py
+++ authentication/utils.py
@@ -4,4 +4,4 @@ class AppTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (text_type(user.is_active)+text_type(user.pk)+text_type(timestamp))
         # return six.text_type(user.username) + six.text_type(user.pk)+six.text_type(timestamp)
-token_generator = AppTokenGenerator()
+account_activation_token = AppTokenGenerator()
--- authentication/views.py
+++ authentication/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import redirect, render
 from django.views import View
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from flask import request
 from validate_email import validate_email
 from django.contrib import messages
 from django.core.mail import EmailMessage
@@ -15,7 +16,8 @@ from django.urls import reverse
 from django.utils.encoding import force_bytes,force_str,DjangoUnicodeDecodeError
 from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
 from django.contrib.sites.shortcuts import get_current_site
-from .utils import token_generator
+from .utils import account_activation_token
+from django.contrib import auth
 # Create your views here.
 
 class EmailValidationView(View):
@@ -75,7 +77,7 @@ class RegistrationView(View):
                 # token
                 uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
                 domain = get_current_site(request).domain
-                link=reverse('activate',kwargs={'uidb64':uidb64,'token':token_generator.make_token(user)})
+                link=reverse('activate',kwargs={'uidb64':uidb64,'token':account_activation_token.make_token(user)})
                 activate_url='http://'+domain+link
                 email_body='Hi'+user.username+'Please use this link to verify your account\n'+activate_url
                 email_subject='Activate your account'
@@ -115,3 +117,30 @@ class VerificationView(View):
 class LoginView(View):
     def get(self,request):
         return render(request,'authentication/login.html')
+    
+    def post(self,request):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if username and password:
+            user = auth.authenticate(username=username,password=password)
+
+            if user:
+                if user.is_activate:
+                    auth.login(request,user)
+                    messages.success(request,'welcome, '+user.username+' you are now logged in')
+                    return redirect('expenses')
+                messages.error(request,'Account is not active,please check your email')
+                return render(request,'authentication/login.html')
+            messages.error(request,'Invalid credentials,try again')
+            return render(request,'authentication/login.html')
+        
+        messages.error(request,'Please fill all fields')
+        return render(request,'authentication/login.html')
+
+
+class LogoutView(View):
+    def post(self,request):
+        auth.logout(request)
+        messages.success(request,'You have been logged out')
+        return redirect('login')
